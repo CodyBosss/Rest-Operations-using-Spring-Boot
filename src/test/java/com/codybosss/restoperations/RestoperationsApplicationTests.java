401544af2a468f9457ca8edb5430c2f505f10e29
SandyCoder <sudhir.bk9@gmail.com>
2024-08-19 22:45:23 +0530

    Initial Comment

---
+++ src/test/java/com/codybosss/restoperations/RestoperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codybosss.restoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
